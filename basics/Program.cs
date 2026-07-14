bc42ed9f6729842f4fe1384ba9e7a993e84532e5
Marky <markdangal5858@gmail.com>
2026-07-14 18:23:21 +0545

    Add project files.

---
+++ basics/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+int[] numbers = { 10, 30, 20, 40, 50 };
+Array.Reverse(numbers);
+Console.WriteLine(string.Join(", ", numbers));
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}
+Array.Clear(numbers, 0, numbers.Length);
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}

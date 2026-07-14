db1512ac3782d04f559da52ff9440e20b1739ba1
Marky <markdangal5858@gmail.com>
2026-07-14 18:23:21 +0545

    Add project files.

---
+++ CSharp-Practice/03-Array/Program.cs
@@ -0,0 +1,39 @@
+﻿//Hands on Array
+
+using System.Diagnostics.CodeAnalysis;
+
+var numbers = new[] { 10, 20, 40, 50, 30 };
+Console.WriteLine(Array.IndexOf(numbers, 50));
+
+bool found = Array.Exists(numbers, x => x == 20+2);
+Console.WriteLine(found);
+
+//multidimensional array
+var number1 = new[,]
+{
+    {10,20,30 },
+    {40,50,60 },
+    {70,80,90 }
+};
+
+Console.WriteLine(number1.Rank);
+
+int[,] matrix = { 
+    { 10,20,30},
+    { 40,50,60}
+    };
+//Console.WriteLine(matrix[2, 2]);
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j= 0;j<matrix.GetLength(1); j++)
+    {
+        Console.WriteLine(matrix[i, j]+ " ");
+    }
+}
+Console.WriteLine("so this below is printed using foreach looping statement");
+
+foreach(int m in matrix)
+{
+    Console.WriteLine(m + " ");
+}
---
+++ CSharp-Practice/04-OOP/Program.cs
@@ -0,0 +1,145 @@
+﻿//classes and objects practice
+
+//access modifier
+using System;
+using System.Diagnostics;
+
+//BankAccount
+//│
+//├── private balance
+//├── Deposit()
+//├── Withdraw()
+//└── ShowBalance()
+
+//class BankAccount
+//{
+//    private double balance;
+
+//    public void Deposit(double amount)
+//    {
+//        if (amount > 0)
+//        {
+//            balance += amount;
+//            Console.WriteLine($"{amount} deposited in the bank acc");
+//        }
+//        else
+//        {
+//            Console.WriteLine("cant deopist the amount less then 0");
+//        }
+//    }
+
+//    public void WithDrawl(double amount)
+//    {
+//        if( balance < amount)
+//        {
+//            Console.WriteLine("The amount cant be withdrawl as it is more than the bank baalnce itself");
+//        }
+//        else
+//        {
+//            balance -= amount;
+//            Console.WriteLine($"{amount} from your bank balance");
+//        }
+//    }
+
+//    public void ShowBalance()
+//    {
+//        Console.WriteLine($"{balance} is the current bank balance");
+//    }
+
+//}
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        BankAccount account= new BankAccount();
+//        account.Deposit(5000);
+//        account.WithDrawl(2000);
+//        account.ShowBalance();
+//        //account.balance = 5000; not possble because accessing the private data members is not possible outside of the class 
+//    }
+//}
+
+
+//Create a C# program to manage employees.
+
+//Requirements
+
+//Create a class named Employee with the following members:
+
+//private double salary
+//protected string department
+//public string Name
+
+//Create the following methods:
+
+//SetSalary(double amount) → Sets the salary if it is greater than 0.
+//ShowSalary() → Displays the salary.
+
+//Now create another class named Manager that inherits from Employee.
+
+//Inside the Manager class:
+
+//Assign "IT Department" to department.
+//Create a method ShowDepartment() that displays the department.
+
+//In Main():
+
+//Create a Manager object.
+//Set the manager's name.
+//Set the salary.
+//Display the salary.
+//Display the department.
+
+//Finally, try accessing salary and department directly from Main() and observe the errors.
+
+using System;
+class Employee
+{
+    private double salary;
+
+    protected string department;
+
+    public string name;
+
+    public void SetSalary(double amount)
+    {
+        if (amount > 0)
+        {
+            salary = amount;
+            Console.WriteLine("salary assigneed succesfully!!!!!!");
+        }
+        else
+        {
+            Console.WriteLine("Amount smaller then or equal  cant be assigned as salary");
+        }
+    }
+    public void ShowSalary()
+    {
+        Console.WriteLine($"{salary} is the salary amount assigned  ");
+    }
+}
+class Manager : Employee
+{
+    public Manager()
+    {
+        department = "IT Department";
+    }
+    public void ShowDepartment()
+    {
+        Console.WriteLine($"The name of the department is:{department}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager emp = new Manager();
+        emp.name = "marky";
+        Console.WriteLine($"The name of the manager is {emp.name}");
+        emp.SetSalary(50000);
+        emp.ShowSalary();
+        emp.ShowDepartment();
+        //emp.department = 5000;
+        //emp.salary=20000;
+    }
+}

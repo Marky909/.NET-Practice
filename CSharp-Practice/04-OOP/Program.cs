24cd6ecd4f16ce815eb4d656fe6ba41edafac631
Marky <markdangal5858@gmail.com>
2026-07-23 08:15:50 +0545

    added classes and objects

--- CSharp-Practice/04-OOP/Program.cs
+++ CSharp-Practice/04-OOP/Program.cs
@@ -1,145 +1,28 @@
-﻿//classes and objects practice
+﻿using System;
 
-//access modifier
-//using System;
-//using System.Diagnostics;
+Console.WriteLine("===== Bank Account Example =====");
 
-//BankAccount
-//│
-//├── private balance
-//├── Deposit()
-//├── Withdraw()
-//└── ShowBalance()
+BankAccount account = new BankAccount();
 
-//class BankAccount
-//{
-//    private double balance;
+account.Deposit(5000);
+account.Withdraw(2000);
+account.ShowBalance();
 
-//    public void Deposit(double amount)
-//    {
-//        if (amount > 0)
-//        {
-//            balance += amount;
-//            Console.WriteLine($"{amount} deposited in the bank acc");
-//        }
-//        else
-//        {
-//            Console.WriteLine("cant deopist the amount less then 0");
-//        }
-//    }
+Console.WriteLine();
 
-//    public void WithDrawl(double amount)
-//    {
-//        if( balance < amount)
-//        {
-//            Console.WriteLine("The amount cant be withdrawl as it is more than the bank baalnce itself");
-//        }
-//        else
-//        {
-//            balance -= amount;
-//            Console.WriteLine($"{amount} from your bank balance");
-//        }
-//    }
+Console.WriteLine("===== Employee Example =====");
 
-//    public void ShowBalance()
-//    {
-//        Console.WriteLine($"{balance} is the current bank balance");
-//    }
+Manager manager = new Manager();
 
-//}
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        BankAccount account= new BankAccount();
-//        account.Deposit(5000);
-//        account.WithDrawl(2000);
-//        account.ShowBalance();
-//        //account.balance = 5000; not possble because accessing the private data members is not possible outside of the class 
-//    }
-//}
+manager.Name = "Marky";
 
+Console.WriteLine($"Manager Name: {manager.Name}");
 
-//Create a C# program to manage employees.
+manager.SetSalary(50000);
 
-//Requirements
+manager.ShowSalary();
 
-//Create a class named Employee with the following members:
+manager.ShowDepartment();
 
-//private double salary
-//protected string department
-//public string Name
-
-//Create the following methods:
-
-//SetSalary(double amount) → Sets the salary if it is greater than 0.
-//ShowSalary() → Displays the salary.
-
-//Now create another class named Manager that inherits from Employee.
-
-//Inside the Manager class:
-
-//Assign "IT Department" to department.
-//Create a method ShowDepartment() that displays the department.
-
-//In Main():
-
-//Create a Manager object.
-//Set the manager's name.
-//Set the salary.
-//Display the salary.
-//Display the department.
-
-//Finally, try accessing salary and department directly from Main() and observe the errors.
-
-using System;
-class Employee
-{
-    private double salary;
-
-    protected string department;
-
-    public string name;
-
-    public void SetSalary(double amount)
-    {
-        if (amount > 0)
-        {
-            salary = amount;
-            Console.WriteLine("salary assigneed succesfully!!!!!!");
-        }
-        else
-        {
-            Console.WriteLine("Amount smaller then or equal  cant be assigned as salary");
-        }
-    }
-    public void ShowSalary()
-    {
-        Console.WriteLine($"{salary} is the salary amount assigned  ");
-    }
-}
-class Manager : Employee
-{
-    public Manager()
-    {
-        department = "IT Department";
-    }
-    public void ShowDepartment()
-    {
-        Console.WriteLine($"The name of the department is:{department}");
-    }
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        Manager emp = new Manager();
-        emp.name = "marky";
-        Console.WriteLine($"The name of the manager is {emp.name}");
-        emp.SetSalary(50000);
-        emp.ShowSalary();
-        emp.ShowDepartment();
-        //emp.department = 5000;
-        //emp.salary=20000;
-    }
-}
+// manager.salary = 50000;      // Error
+// manager.department = "HR";   // Error

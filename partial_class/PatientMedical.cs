ea947f9d78b817d34be76d3c2c22b53e71dbfdaf
Marky <markdangal5858@gmail.com>
2026-07-26 07:38:42 +0545

    Made small projects in partial class

---
+++ partial_class/PatientMedical.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace partial_class
+{
+    public partial class Patient
+    {
+        public string BloodGroup { get; set; } = "";
+        public string Disease { get; set; } = "";
+        public string Allergies { get; set; } = "";
+    }
+}

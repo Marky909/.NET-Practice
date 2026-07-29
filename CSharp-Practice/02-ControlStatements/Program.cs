935f7e71a5be85723f518ff791672cf80a45e2c4
Marky <markdangal5858@gmail.com>
2026-07-29 19:28:52 +0545

    added if , if ..else and else if and also switch statment

--- CSharp-Practice/02-ControlStatements/Program.cs
+++ CSharp-Practice/02-ControlStatements/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, Mark");
+﻿using System;
+
+namespace _02_ControlStatements;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Run ONE at a time
+
+        IfElseTesting.Run();
+
+        // SwitchCase.Run();
+
+        SwitchDemo.Run();
+    }
+}

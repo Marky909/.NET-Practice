d9d508beb9409cf0bd9381e820cd17977d91eca5
Marky <markdangal5858@gmail.com>
2026-07-30 21:05:35 +0545

    created calculator using interface

---
+++ CSharp-Practice/CalculatorUsingInterface/Program.cs
@@ -0,0 +1,13 @@
+﻿using CalculatorUsingInterface;
+
+IOperation add = new Add();
+Console.WriteLine(add.Calculate(5, 6));
+
+IOperation sub = new Subtract();
+IOperation mul = new Multiply();
+IOperation div = new Divide();
+
+Console.WriteLine(sub.Calculate(5, 3));
+Console.WriteLine(mul.Calculate(5, 3));
+Console.WriteLine(div.Calculate(5, 3));
+

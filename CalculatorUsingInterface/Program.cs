3c30df91da97f3f63193bff6c272d3027dc5fca5
Marky <markdangal5858@gmail.com>
2026-08-02 19:30:22 +0545

    modifed the divide operartion handled the division by 0

--- CalculatorUsingInterface/Program.cs
+++ CalculatorUsingInterface/Program.cs
@@ -1,13 +1,21 @@
 ﻿using CalculatorUsingInterface;
 
 IOperation add = new Add();
-Console.WriteLine(add.Calculate(5, 6));
-
 IOperation sub = new Subtract();
 IOperation mul = new Multiply();
 IOperation div = new Divide();
 
-Console.WriteLine(sub.Calculate(5, 3));
-Console.WriteLine(mul.Calculate(5, 3));
-Console.WriteLine(div.Calculate(5, 3));
+Console.Write("Enter the first number: ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+double num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The result of addition of {num1} and {num2} is {add.Calculate(num1, num2)}");
+Console.WriteLine($"The result of subtraction of {num1} and {num2} is {sub.Calculate(num1, num2)}");
+Console.WriteLine($"The result of Multiplication of {num1} and {num2} is {mul.Calculate(num1, num2)}");
+Console.WriteLine($"The result of Division of {num1} and {num2} is {div.Calculate(num1, num2)}");
+//Console.WriteLine(sub.Calculate(num1, num2));
+//Console.WriteLine(mul.Calculate(num1, num2));
+//Console.WriteLine(div.Calculate(num1, num2));
 

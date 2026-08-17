5a4b33d1daab423b805ba52e20963b1dbdba619c
Marky <markdangal5858@gmail.com>
2026-08-17 16:39:44 +0545

    Created the caetgory controller

---
+++ Api_practice/Api_practice/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}

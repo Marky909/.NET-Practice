1c1d92cefbb4e21cddda2ab66430a446244dccf8
Marky <markdangal5858@gmail.com>
2026-08-17 17:16:11 +0545

    created list and added few redundant data so to demonstarte the get method

--- Api_practice/Api_practice/Controllers/CategoryController.cs
+++ Api_practice/Api_practice/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Api_practice.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Api_practice.Controllers
 {
@@ -7,5 +9,25 @@ namespace Api_practice.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        public static List<Category> categories = new List<Category>
+        {
+            new() {Id=1,BrandName="Samsung",ProductName="A27",Quantity=5},
+            new() {Id=2,BrandName="Samsung",ProductName="A27",Quantity=5},
+            new() {Id=3,BrandName="Samsung",ProductName="A27",Quantity=5},
+            new() {Id=4,BrandName="Samsung",ProductName="A27",Quantity=5},
+            new() {Id=5,BrandName="Samsung",ProductName="A27",Quantity=5},
+        };
+
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return categories;
+        }
+
+        [HttpPost]
+        public void  Post(Category category)
+        {
+            categories.Add(category);
+        }
     }
 }

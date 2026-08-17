545656e2623b7435b03ae1db533b97ee79e59ca8
Marky <markdangal5858@gmail.com>
2026-08-17 16:46:06 +0545

    created the firlds to store the data

---
+++ Api_practice/Api_practice/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_practice.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Category : ControllerBase
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; } = "";
+        public string ProductName{ get; set; } = "";
+        public int Quantity { get; set; }
+    }
+}

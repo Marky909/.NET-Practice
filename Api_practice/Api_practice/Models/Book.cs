efcd3171db005c2ed34b64b418538857a65bef38
Marky <markdangal5858@gmail.com>
2026-08-22 08:09:28 +0545

    Added BookModel so that it can store the fields

---
+++ Api_practice/Api_practice/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstAPI.Models
+{
+    public class Book
+    {
+        // Unique identifier for each book
+        public int Id { get; set; }
+
+        // Initialized as null! (or empty string) to suppress C# nullable warnings [5]
+        public string Title { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public int YearPublished { get; set; }
+    }
+}
